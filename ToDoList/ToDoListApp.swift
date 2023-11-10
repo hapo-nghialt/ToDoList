b4264ca7ecb2a4d3b07e5c288a2c43f7ccf38f83
hapo-nghialt <nghia.lt@haposoft.com>
2023-11-10 15:20:18 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nghĩa Lê Trọng on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
